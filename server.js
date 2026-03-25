0fc1d40a5f45b20fca774d2ed3fdd4cce79003cd
dipghosh <dip.ghosh@bracits.com>
2026-03-25 15:58:11 +0600

    building website node

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', function (req, res) {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
