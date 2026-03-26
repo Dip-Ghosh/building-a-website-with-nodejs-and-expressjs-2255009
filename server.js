42cb1d3548ff2be040fa49eca9e170716b835652
Dip Ghosh <dipnsucse@gmail.com>
2026-03-26 20:54:33 +0600

    using templating

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './static/index.html'));
+  res.render('pages/index', {
+    pageTitle: 'Node.js  wolcome',
+  });
+  // res.sendFile(path.join(__dirname, './static/index.html'));
 });
 
 app.get('/speakers', function (req, res) {
